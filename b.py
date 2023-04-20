3338c55e26ee760f2baaa3f5db83768d304c4fe6
skogk4tt <69017227+skogk4tt@users.noreply.github.com>
2023-04-19 22:46:48 -0400

    Update b.py

--- b.py
+++ b.py
@@ -1,14 +1,22 @@
 import os
 import shutil
 import time
-import glob
 
-source_folder = os.path.expanduser('~/dotfiles/config/*')
+source_folder = os.path.expanduser('~/dotfiles/config/')
 target_folder = os.path.expanduser('~/.config/')
 
-for file_path in glob.glob(source_folder):
-    try:
-        shutil.copy2(file_path, target_folder)
-    except Exception as e:
-        print(f"Error copying file {file_path}: {str(e)}")
-    time.sleep(1)
+for dirpath, dirnames, filenames in os.walk(source_folder):
+    # Creamos la carpeta de destino si no existe
+    target_dir = os.path.join(target_folder, os.path.relpath(dirpath, source_folder))
+    os.makedirs(target_dir, exist_ok=True)
+
+    for file_name in filenames:
+        source_file_path = os.path.join(dirpath, file_name)
+        target_file_path = os.path.join(target_dir, file_name)
+
+        try:
+            shutil.copy2(source_file_path, target_file_path)
+        except Exception as e:
+            print(f"Error copying file {source_file_path}: {str(e)}")
+        
+        time.sleep(1)

9fda3c2c4d841cf61b408cdf093076c1a6257b82
skogk4tt <skogk4tt@gmail.com>
2023-04-19 22:56:58 -0400

    cosas

--- b.py
+++ b.py
@@ -1,14 +1,15 @@
 import os
 import shutil
 import time
-import glob
 
-source_folder = os.path.expanduser('~/dotfiles/config/*')
+source_folder = os.path.expanduser('~/dotfiles/config/')
 target_folder = os.path.expanduser('~/.config/')
 
-for file_path in glob.glob(source_folder):
-    try:
-        shutil.copy2(file_path, target_folder)
-    except Exception as e:
-        print(f"Error copying file {file_path}: {str(e)}")
-    time.sleep(1)
+try:
+    shutil.copytree(source_folder, target_folder, dirs_exist_ok=True)
+except Exception as e:
+    print(f"Error copying files: {str(e)}")
+    exit(1)
+
+print("Files copied successfully!")
+time.sleep(3)

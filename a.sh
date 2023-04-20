9cb728a88084b108f1e9d91739d12219ab7b781a
skogk4tt <69017227+skogk4tt@users.noreply.github.com>
2023-04-19 21:47:55 -0400

    Create a.sh

---
+++ a.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+for archivos in ~/dotfiles/config/*; do
+  cp -R "${archivos}" ~/.config/
+  if [ $? -eq 0 ]; then
+	printf "%s%s%s folder copied succesfully!%s\n" "${BLD}" "${CGR}" "${archivos}" "${CNC}"
+	sleep 1
+  else
+	printf "%s%s%s failed to been copied, you must copy it manually%s\n" "${BLD}" "${CRE}" "${archivos}" "${CNC}"
+	sleep 1
+  fi
+done
